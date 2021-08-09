107b6ba8fe8ec4e2e485aeb57074aadf01c0781b
Васильев <workgames@mail.ru>
2021-08-09 14:58:06 +0500

    fix: webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,79 +9,79 @@ const isDev = !isProd;
 
 const fileName = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
 const jsLoader = () => {
-  const loaders = [
-    {
-      loader: "babel-loader",
-      options: {
-        presets: ['@babel/preset-env']
-      }
+    const loaders = [
+        {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+    ];
+
+    if (isDev) {
+        loaders.push('eslint-loader')
     }
-  ];
 
-  if (isDev) {
-    loaders.push('eslint-loader')
-  }
-
-  return loaders;
+    return loaders;
 }
 console.log('isProd: ', isProd)
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'), // где лежат все исходники приложения
-  mode: "development",
-  target: isDev ? "web" : "browserslist",
-  entry: ['@babel/polyfill', './index.js'],
-  output: {
-    filename: fileName('js'),
-    path: path.resolve(__dirname, 'dist')
-  },
-  resolve: {
-    extensions: ['.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src/core'),
-    }
-  },
-  devtool: isDev ? 'source-map' : false,
-  devServer: {
-    port: 3000,
-    hot: isDev,
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: fileName('css'),
-    }),
-    new CleanWebpackPlugin(),
-    new HtmlWebPackPlugin({
-      template: "index.html",
-      minify: {
-        removeComments: isProd,
-        collapseWhitespace: isProd,
-      }
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: path.resolve(__dirname, 'src/favicon.ico'), to: path.resolve(__dirname, 'dist') },
-      ],
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: jsLoader()
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          "css-loader",
-          "sass-loader",
-        ],
-      },
+    context: path.resolve(__dirname, 'src'), // где лежат все исходники приложения
+    mode: "development",
+    target: isDev ? "web" : "browserslist",
+    entry: ['@babel/polyfill', './index.js'],
+    output: {
+        filename: fileName('js'),
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'src/core'),
+        }
+    },
+    devtool: isDev ? 'source-map' : false,
+    devServer: {
+        port: 3000,
+        hot: isDev,
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: fileName('css'),
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebPackPlugin({
+            template: "index.html",
+            minify: {
+                removeComments: isProd,
+                collapseWhitespace: isProd,
+            }
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, 'src/favicon.ico'), to: path.resolve(__dirname, 'dist') },
+            ],
+        }),
     ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: jsLoader()
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
 }
